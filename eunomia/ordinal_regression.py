d482af2ad4eaff5d57dff214f86b2a046f96894d
Ross Diener <ross.diener@shopify.com>
2017-12-12 09:01:30 -0500

    Inference

--- eunomia/ordinal_regression.py
+++ eunomia/ordinal_regression.py
@@ -5,6 +5,8 @@ from numpy.linalg import inv
 from scipy import optimize
 from scipy.linalg import block_diag
 
+from scipy import stats
+
 
 def logistic(z):
     positive_z = z > 0
@@ -21,30 +23,25 @@ class OrdinalRegression():
         pass
 
     def fit(self, X, y):
-        X, y = self.clean(X, y)
+        X, y = self._clean(X, y)
 
         beta_guess = np.zeros(self.n_attributes)
         gamma_guess = np.ones(self.n_classes - 1)
         bounds = [(None, None)] * (self.n_attributes + 1) + [(0, None)] * (self.n_classes - 2)
 
         optimization = optimize.minimize(
-            self.log_likelihood, 
+            self._log_likelihood, 
             np.append(beta_guess, gamma_guess),
             args=(X, y),
             bounds=bounds
         )
 
-        self.set_parameters(optimization.x)
-        self.se_ = self.standard_errors(X, y)
+        self._set_coefficients(optimization.x)
+        self.se_ = self._standard_errors(X, y)
+        self.p_values_ = self._p_values()
         return self
 
-    def set_parameters(self, optimization_x):
-        self.gamma_ = optimization_x[self.n_attributes:]
-        self.beta_ = optimization_x[:self.n_attributes]
-        self.alpha_ = np.cumsum(self.gamma_)
-        self.coef_ = np.append(self.beta_, self.alpha_)
-
-    def clean(self, X, y):
+    def _clean(self, X, y):
         X = np.asarray(X)
         self.N, self.n_attributes = X.shape
 
@@ -53,21 +50,10 @@ class OrdinalRegression():
         self.n_classes = len(y_values)
         y_range = np.arange(1, self.n_classes + 1)
         y = np.vectorize(dict(zip(y_values, y_range)).get)(y)
-        
+
         return X, y
 
-    def standard_errors(self, X, y):
-        hessian_function = Hessian(self.log_likelihood, method='forward')
-        H = hessian_function(np.append(self.beta_, self.gamma_), X, y)
-        J = self.jacobian()
-        return np.sqrt(np.diagonal(J.dot(inv(H)).dot(J.T)))
-
-    def jacobian(self):
-        upper_left_diagonal = np.identity(self.n_attributes)
-        lower_right_triangular = np.tril(np.ones((self.n_classes - 1, self.n_classes-1)))
-        return block_diag(upper_left_diagonal, lower_right_triangular)
-
-    def log_likelihood(self, coefficients, X, y):
+    def _log_likelihood(self, coefficients, X, y):
         beta = coefficients[:self.n_attributes]
         gamma = coefficients[self.n_attributes:]
 
@@ -78,3 +64,28 @@ class OrdinalRegression():
         z_plus = _alpha[y] - X.dot(beta)
         z_minus = _alpha[y-1] - X.dot(beta)
         return - 1.0 * np.sum(np.log(logistic(z_plus) - logistic(z_minus)))
+
+    def _set_coefficients(self, optimization_x):
+        self.gamma_ = optimization_x[self.n_attributes:]
+        self.beta_ = optimization_x[:self.n_attributes]
+        self.alpha_ = np.cumsum(self.gamma_)
+        self.coef_ = np.append(self.beta_, self.alpha_)
+
+    def _standard_errors(self, X, y):
+        hessian_function = Hessian(self._log_likelihood, method='forward')
+        H = hessian_function(np.append(self.beta_, self.gamma_), X, y)
+        J = self._jacobian()
+        return np.sqrt(np.diagonal(J.dot(inv(H)).dot(J.T)))
+
+    def _jacobian(self):
+        upper_left_diagonal = np.identity(self.n_attributes)
+        lower_right_triangular = np.tril(np.ones((self.n_classes - 1, self.n_classes-1)))
+        return block_diag(upper_left_diagonal, lower_right_triangular)
+
+    def _z_values(self):
+        return self.coef_ / self.se_
+
+    def _p_values(self):
+        z_magnitudes = np.abs(self._z_values())
+        p_values = 1 - stats.norm.cdf(z_magnitudes) + stats.norm.cdf(-z_magnitudes)
+        return p_values
--- tests/test_ordinal_regression.py
+++ tests/test_ordinal_regression.py
@@ -5,8 +5,10 @@ import pytest
 
 from eunomia.ordinal_regression import logistic
 from eunomia.ordinal_regression import OrdinalRegression
-from numpy.testing import assert_array_almost_equal
+from numpy.testing import assert_allclose
 from numpy.testing import assert_array_equal
+
+from scipy import stats
 
 
 eunomia_dir = os.path.dirname(os.path.realpath(__file__))
@@ -41,14 +43,28 @@ class TestOrdinalRegression():
         ordinal_regression.fit(X_ucla, y_ucla)
         
         expected_coef_ = np.array([1.04769, -0.05879, 0.61594, 2.20391, 4.29936])
-        assert_array_almost_equal(ordinal_regression.coef_, expected_coef_, decimal=3)
+        assert_allclose(ordinal_regression.coef_, expected_coef_, rtol=0.01)
 
     def test_ucla_se_(self, X_ucla, y_ucla):              
         ordinal_regression = OrdinalRegression()
         ordinal_regression.fit(X_ucla, y_ucla)
 
         expected_se_ = np.array([0.2658, 0.2979, 0.2606, 0.7795, 0.8043])
-        assert_array_almost_equal(ordinal_regression.se_, expected_se_, decimal=3)
+        assert_allclose(ordinal_regression.se_, expected_se_, rtol=0.01)
+
+    def test_ucla_z_values_(self, X_ucla, y_ucla):              
+        ordinal_regression = OrdinalRegression()
+        ordinal_regression.fit(X_ucla, y_ucla)
+
+        expected_z_values_ = np.array([3.9418, -0.1974, 2.3632, 2.8272, 5.3453])
+        assert_allclose(ordinal_regression._z_values(), expected_z_values_, rtol=0.01)
+
+    def test_ucla_p_values_(self, X_ucla, y_ucla):              
+        ordinal_regression = OrdinalRegression()
+        ordinal_regression.fit(X_ucla, y_ucla)
+
+        expected_p_values_ = np.array([8.087e-05, 8.435e-01, 1.812e-02, 4.696e-03, 9.027e-08])
+        assert_allclose(ordinal_regression.p_values_, expected_p_values_, rtol=0.01)
 
     def test_jacobian(self):
         ordinal_regression = OrdinalRegression()
@@ -62,7 +78,7 @@ class TestOrdinalRegression():
             [0., 0., 1., 1., 1., 0.],
             [0., 0., 1., 1., 1., 1.]
         ])
-        assert_array_equal(expected, ordinal_regression.jacobian())
+        assert_array_equal(expected, ordinal_regression._jacobian())
 
     def test_log_likelihood(self):
         ordinal_regression = OrdinalRegression()
@@ -70,7 +86,7 @@ class TestOrdinalRegression():
         y = np.array([1,1,2])
         coefficients = np.array([1.0, 1.0])
         ordinal_regression.n_attributes = 1
-        actual = ordinal_regression.log_likelihood(coefficients, X, y)
+        actual = ordinal_regression._log_likelihood(coefficients, X, y)
         expected = - 3.0 * np.log(0.5)
         assert actual == expected
 
@@ -78,7 +94,7 @@ class TestOrdinalRegression():
         ordinal_regression = OrdinalRegression()
         y = np.array([1,3,5])
         expected = np.array([1,2,3])
-        _, actual = ordinal_regression.clean(np.empty((1,1)), y)
+        _, actual = ordinal_regression._clean(np.empty((1,1)), y)
         assert_array_equal(actual, expected)
         assert ordinal_regression.N == 1
         assert ordinal_regression.n_attributes == 1
