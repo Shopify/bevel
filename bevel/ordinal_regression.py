1ba2383de41a2f7ac13e01b7865a24493240baf3
Ross Diener <ross.diener@shopify.com>
2017-12-16 10:01:05 -0500

    Cleaner code

--- bevel/ordinal_regression.py
+++ bevel/ordinal_regression.py
@@ -27,7 +27,7 @@ class OrdinalRegression():
         self.maxiter = maxiter
 
     def fit(self, X, y):
-        X_data, y_data = self._clean(X, y)
+        X_data, y_data = self._prepare(X, y)
 
         beta_guess = np.zeros(self.n_attributes)
         gamma_guess = np.ones(self.n_classes - 1)
@@ -47,8 +47,8 @@ class OrdinalRegression():
             warnings.warn(warning_message, RuntimeWarning)
 
         self._set_coefficients(optimization.x)
-        self.se_ = self._standard_errors(X_data, y_data)
-        self.p_values_ = self._p_values()
+        self.se_ = self._compute_standard_errors(X_data, y_data)
+        self.p_values_ = self._compute_p_values()
         return self
 
     @property
@@ -63,13 +63,13 @@ class OrdinalRegression():
         """
 
         alpha_std_normal = norm.ppf((1. + self.alpha) / 2.)
-        index = self.attribute_names or ['column_' + str(i) for i in range(self.n_attributes)]
         df = pd.DataFrame(index=self.attribute_names)
         df['coef'] = self.beta_
         df['se(coef)'] = self.se_[:self.n_attributes]
         df['p'] = self.p_values_[:self.n_attributes]
-        df['lower %.2f' % self.alpha] = self.beta_ - alpha_std_normal * self.se_[:self.n_attributes]
-        df['upper %.2f' % self.alpha] = self.beta_ + alpha_std_normal * self.se_[:self.n_attributes]
+        conf_interval = alpha_std_normal * self.se_[:self.n_attributes]
+        df['lower %.2f' % self.alpha] = self.beta_ - conf_interval
+        df['upper %.2f' % self.alpha] = self.beta_ + conf_interval
         return df
 
     def print_summary(self):
@@ -102,12 +102,23 @@ class OrdinalRegression():
               end='\n\n')
         return
 
-    def _clean(self, X, y):
+    def predict(self, X):
+        if X.ndim == 1:
+            X = X[None, :]
+
+        bounded_alpha = self._bounded_alpha(self.alpha_)
+        z = (bounded_alpha - np.dot(X, self.beta_[:, None]))
+        cumulative_dist = logistic(z)
+        raw_predictions = np.argmax(np.diff(cumulative_dist), axis=1) + 1
+        return np.vectorize(self.inverse_y_dict.get)(raw_predictions)
+
+    def _prepare(self, X, y):
+        X_data = np.asarray(X)
+        self.N, self.n_attributes = X_data.shape
         if isinstance(X, pd.DataFrame):
             self.attribute_names = X.columns.tolist()
-
-        X_data = np.asarray(X)
-        self.N, self.n_attributes = X.shape
+        else:
+            self.attribute_names = ['column_' + str(i) for i in range(self.n_attributes)]
 
         y_data = np.asarray(y).astype(np.int)
         y_values = np.sort(np.unique(y_data))
@@ -122,13 +133,9 @@ class OrdinalRegression():
     def _log_likelihood(self, coefficients, X_data, y_data):
         beta = coefficients[:self.n_attributes]
         gamma = coefficients[self.n_attributes:]
-
-        _alpha = np.cumsum(gamma)
-        _alpha = np.insert(_alpha, 0, -np.inf)
-        _alpha = np.append(_alpha, np.inf)
-
-        z_plus = _alpha[y_data] - X_data.dot(beta)
-        z_minus = _alpha[y_data-1] - X_data.dot(beta)
+        bounded_alpha = self._bounded_alpha(np.cumsum(gamma))
+        z_plus = bounded_alpha[y_data] - X_data.dot(beta)
+        z_minus = bounded_alpha[y_data-1] - X_data.dot(beta)
         return - 1.0 * np.sum(np.log(logistic(z_plus) - logistic(z_minus)))
 
     def _set_coefficients(self, optimization_x):
@@ -137,33 +144,27 @@ class OrdinalRegression():
         self.alpha_ = np.cumsum(self.gamma_)
         self.coef_ = np.append(self.beta_, self.alpha_)
 
-    def _standard_errors(self, X_data, y_data):
+    def _compute_standard_errors(self, X_data, y_data):
         hessian_function = Hessian(self._log_likelihood, method='forward')
         H = hessian_function(np.append(self.beta_, self.gamma_), X_data, y_data)
-        J = self._jacobian()
+        J = self._compute_jacobian()
         return np.sqrt(np.diagonal(J.dot(inv(H)).dot(J.T)))
 
-    def _jacobian(self):
+    def _compute_jacobian(self):
         upper_left_diagonal = np.identity(self.n_attributes)
-        lower_right_triangular = np.tril(np.ones((self.n_classes - 1, self.n_classes-1)))
+        lower_right_triangular = np.tril(np.ones((self.n_classes - 1, self.n_classes - 1)))
         return block_diag(upper_left_diagonal, lower_right_triangular)
 
-    def _z_values(self):
+    def _compute_z_values(self):
         return self.coef_ / self.se_
 
-    def _p_values(self):
-        z_magnitudes = np.abs(self._z_values())
+    def _compute_p_values(self):
+        z_magnitudes = np.abs(self._compute_z_values())
         p_values = 1 - norm.cdf(z_magnitudes) + norm.cdf(-z_magnitudes)
         return p_values
 
-    def predict(self, X):
-        if X.ndim == 1:
-            X = X[None, :]
-
-        alpha = self.alpha_
-        alpha = np.insert(alpha, 0, -np.inf)
-        alpha = np.append(alpha, np.inf)
-        z = (alpha - np.dot(X, self.beta_[:, None]))
-        cumulative_dist = logistic(z)
-        raw_predictions = np.argmax(np.diff(cumulative_dist), axis=1) + 1
-        return np.vectorize(self.inverse_y_dict.get)(raw_predictions)
+    @staticmethod
+    def _bounded_alpha(alpha):
+        _alpha = np.insert(alpha, 0, -np.inf)
+        _alpha = np.append(_alpha, np.inf)
+        return _alpha
