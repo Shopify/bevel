c8cc8c7157eb2b3db6fd5173fcd9f7353786f4fd
Ross Diener <ross.diener@shopify.com>
2018-11-14 14:13:46 -0500

    Add pivot proportions

---
+++ bevel/df_utils.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def pivot_proportions(df, groups, responses, weights=1):
+    pivot_data = df[[groups, responses]].assign(weights=weights)
+    pivoted_counts = pd.pivot_table(
+        pivot_data,
+        columns=groups,
+        index=responses,
+        aggfunc='sum'
+    )
+    pivoted_counts = pivoted_counts['weights'].sort_index(axis=1)
+    return (pivoted_counts / pivoted_counts.sum()).fillna(0)
+
+
